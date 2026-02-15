9a91883c5fc6e36baf74f6a2dbb2152cff67957a
Leandro-De <yleandrovarela@gmail.comgit config --global user.email yleandrovarela@gmail.comgit config --global user.email yleandrovarela@gmail.com>
2026-02-15 18:34:39 -0500

    mas productos

--- app.js
+++ app.js
@@ -222,6 +222,84 @@ const products = [
         img: "https://http2.mlstatic.com/D_NQ_NP_2X_700007-MCO96514222043_102025-F.webp"
       },
       {
+        type: "celular",
+        name: "Samsung Galaxy A16 5G",
+        specs: "256GB / 8G RAM",
+        img: "https://http2.mlstatic.com/D_NQ_NP_2X_909065-MLA96870019085_102025-F.webp"
+      },
+      {
+        type: "celular",
+        name: "Tecno Go 1S",
+        specs: "64GB / 2G RAM",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSsySS2rKRCY4ayIcppCHa2fH7sEGH7J8DA-setZdEhxTygaP5wnekOUBj5NSzEt2vYTHqIARNHLFmIwK0kmeVqVx0J4iDX2R5sDx8PpHy5IOegAMQtgQGi"
+      },
+      {
+        type: "celular",
+        name: "Honor X9c",
+        specs: "256GB / 8G RAM",
+        img: "https://http2.mlstatic.com/D_NQ_NP_2X_923349-MLA99452707398_112025-F.webp"
+      },
+      {
+        type: "celular",
+        name: "Samsung Galaxy A17 4G",
+        specs: "128GB / 4G RAM",
+        img: "https://http2.mlstatic.com/D_NQ_NP_2X_716271-MLA99853447409_112025-F.webp"
+      },
+      {
+        type: "celular",
+        name: "Honor X5c",
+        specs: "64GB / 2G RAM",
+        img: "https://http2.mlstatic.com/D_NQ_NP_2X_708726-MLA95728312284_102025-F.webp"
+      },
+      {
+        type: "nevera",
+        name: "Minibar Chanllenger",
+        specs: "121 litros",
+        img: "https://www.alkosto.com/medias/7705191043760-004-750Wx750H?context=bWFzdGVyfGltYWdlc3wyNTIzNnxpbWFnZS93ZWJwfGFHWmtMMmhtTWk4eE5ETTJOVE01TWpBME5EQTJNaTgzTnpBMU1Ua3hNRFF6TnpZd1h6QXdORjgzTlRCWGVEYzFNRWd8NmZkNzRkZDU2NDMxZGE2ZGFiMTU2YjEwZTU3YmYxMmJhMDZlMGM5YmQ5MjYxNTFjYThlOTAwYzVjYjlmZTAyNA"
+      },
+      {
+        type: "nevera",
+        name: "Nevera Challenger CR249",
+        specs: "249 Litros",
+        img: "https://challengerco.vteximg.com.br/arquivos/ids/168998-1000-1000/7705191041803_001.jpg?v=638943461363770000"
+      },
+      {
+        type: "lavadora",
+        name: "Lavadora Whirlpool ",
+        specs: "Carga Superior 20 Kilos",
+        img: "https://www.alkosto.com/medias/7708228051554-003-750Wx750H?context=bWFzdGVyfGltYWdlc3w5MjA2fGltYWdlL3dlYnB8YUdFM0wyZ3lZUzh4TlRBME5Ua3pOakl4T0RFME1pODNOekE0TWpJNE1EVXhOVFUwWHpBd00xODNOVEJYZURjMU1FZ3w2NTI1MjQwYjU5OTZhODdlZTEwMTg2MWMyMzdlOGQzY2M0YmEyN2RlODgzMWI4ZTJiYTk5ZGUwMjBlN2FkMDI0"
+      },
+      {
+        type: "lavadora",
+        name: "Lavadora LG semiautomática",
+        specs: "Superior 15 Kilos",
+        img: "https://www.alkosto.com/medias/8806096035610-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wxMjY2MHxpbWFnZS93ZWJwfGFHSmhMMmczTXk4eE5EUXpNRFkzT0RNNE5EWTNNQzg0T0RBMk1EazJNRE0xTmpFd1h6QXdNVjgzTlRCWGVEYzFNRWd8ZDExMTdlNTViNTg2ZDA5NTc4MWU3YjkyNGFhYmNlMTFjNTM2NTg2YzE4YTUyZmE5OTIxZjAwMmViMDhhNjZiYg"
+      },
+      {
+        type: "freidora",
+        name: "Freidora de Aire Kalley ",
+        specs: "7.5 Litros",
+        img: "https://www.alkosto.com/medias/7705946474665-001-750Wx750H?context=bWFzdGVyfGltYWdlc3wyMTc4OHxpbWFnZS93ZWJwfGFHTmxMMmhsWVM4eE5UVXdNamMxTURJMU16QTROaTgzTnpBMU9UUTJORGMwTmpZMVh6QXdNVjgzTlRCWGVEYzFNRWd8Yzg5YTNjODJmZGJhODU0MzZhZjI2ZjM0YjIyNzI3ZDYxODVlNjRkNzc0NjU3NjlhMmJkNTZkNWUwYjg1MDdkZA"
+      },
+      {
+        type: "estufa",
+        name: "Estufa de mesa ABBA",
+        specs: "4 puestos Gas Propano",
+        img: "https://www.alkosto.com/medias/7706060001263-001-750Wx750H?context=bWFzdGVyfGltYWdlc3w0NDUxNnxpbWFnZS93ZWJwfGFESmhMMmc0WVM4eE5USXlOREEzTlRneE1qZzVOQzgzTnpBMk1EWXdNREF4TWpZelh6QXdNVjgzTlRCWGVEYzFNRWd8OGQzYmJkODkxZjY1ODQyYTE4YzY4Njc4MTVhNzYwNDJhOTY3ZDcwZDkxYTM3OWIzMDk2M2YzMjljM2JjYmFlZQ"
+      },
+      {
+        type: "estufa",
+        name: "Estufa con gabinete Haceb",
+        specs: "4 puestos Gas Propano",
+        img: "https://hacebco.vtexassets.com/arquivos/ids/179504-1600-1600?v=638762073218600000&width=1600&height=1600&aspect=true"
+      },
+      {
+        type: "parlante",
+        name: "Barra de Sonido KALLEY K-ABS120W",
+        specs: "Con bajo",
+        img: "https://www.alkosto.com/medias/7705946780063-001-750Wx750H?context=bWFzdGVyfGltYWdlc3w3ODA2fGltYWdlL3dlYnB8YUdReUwyZ3dNUzh4TkRNd016QTVNRE0wTXprMk5pODNOekExT1RRMk56Z3dNRFl6WHpBd01WODNOVEJYZURjMU1FZ3xkOWUzNTRjNmY3NDk4MzIxZTA3YzkzYTY4YWVkNTY0N2UzYWEwMGQ1ZDJmNzlmOTIxMDIzNWVjZGMyMGUyMmUw"
+      },
+      {
         type: "nevera",
         name: "Nevera Frost Challenger ",
         specs: "256 Litros Titanium CR256",
@@ -291,7 +369,7 @@ const products = [
         type: "nevera",
         name: "Minibar Kalley",
         specs: "93 Litros",
-        img: "https://http2.mlstatic.com/D_NQ_NP_2X_614466-MLA99989587163_112025-F.webp"
+        img: "https://www.alkosto.com/medias/7705946916370-001-750Wx750H?context=bWFzdGVyfGltYWdlc3w1MDg2fGltYWdlL3dlYnB8YUROaUwyZzVaQzh4TkRNeU1qWXlOelkzTkRFME1pODNOekExT1RRMk9URTJNemN3WHpBd01WODNOVEJYZURjMU1FZ3xlNTBlY2JkY2IxMzVkNjMxMmQ1NjAyNzAzNDY2MDE4ZWI2MzNmYzY1OTNiYjVmOTIzMTUzMDJmNTVmNjA0MTg2"
       },
       {
         type: "parlante",
@@ -436,6 +514,18 @@ const products = [
         name: "Horno Microondas Kalley",
         specs: "6 programas la función indicada para tus comidas",
         img: "https://http2.mlstatic.com/D_NQ_NP_2X_909655-MLA51562098840_092022-F.webp"
+      },
+            {
+        type: "scooter",
+        name: "Scooter Xiaomi elite ",
+        specs: "¡Precio especial de super oferta!",
+        img: "https://i02.appmifile.com/699_item_co/25/03/2025/cc687cc5e593926a1db971e15a2ceda1.png?thumb=1&f=webp&q=85"
+      },
+            {
+        type: "scooter",
+        name: "Bicicleta Forza RIN 29",
+        specs: "Color azul",
+        img: "https://electrojaponesa.vtexassets.com/arquivos/ids/203321-1200-auto?v=638902898708300000&width=1200&height=auto&aspect=true"
       }
     ];
 
